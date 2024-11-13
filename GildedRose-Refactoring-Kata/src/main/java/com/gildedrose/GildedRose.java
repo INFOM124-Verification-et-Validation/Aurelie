98d7c0ec7da21db5a110f7242a3aeaa341475d1b
AurelieGnt <genot.aurelie.7@gmail.com>
2024-11-13 15:32:50 +0100

    Optimisation en fonctions

--- GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/GildedRose.java
@@ -7,52 +7,56 @@ class GildedRose {
     public GildedRose(Item[] items) {
         this.items = items;
     }
+    public void decreaseQuality(Item item) {
+        if (item.quality > 0 && !item.name.equals(SULFURAS)) {
+            item.quality = item.quality - 1;
+        }
+    }
+    public void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    private void increaseQualityIfSellInLessThan(Item item, int sellInThreshold) {
+        if (item.sellIn < sellInThreshold && item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    private void updateQualityForExpiredItem(Item item) {
+        if (!item.name.equals("Aged Brie")) {
+            if (!item.name.equals(BACKSTAGE)) {
+                decreaseQuality(item);
+            } else {
+                item.quality = 0;
+            }
+        } else {
+            increaseQuality(item);
+        }
+    }
+
 
     public void updateQuality() {
         for (Item item : items) {
             if (!item.name.equals("Aged Brie")
                     && !item.name.equals(BACKSTAGE)) {
-                if (item.quality > 0 && !item.name.equals(SULFURAS)) {
-                    item.quality = item.quality - 1;
-                }
+                decreaseQuality(item);
 
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
+                increaseQuality(item);
                     if (item.name.equals(BACKSTAGE)) {
-                        if (item.sellIn < 11 && item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-
-
-                        if (item.sellIn < 6 && item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-
+                        increaseQualityIfSellInLessThan(item, 11);
+                        increaseQualityIfSellInLessThan(item, 6);
                     }
                 }
-            }
 
             if (!item.name.equals(SULFURAS)) {
                 item.sellIn = item.sellIn - 1;
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals(BACKSTAGE)) {
-                        if (item.quality > 0 && !item.name.equals(SULFURAS)) {
-                            item.quality = item.quality - 1;
-                        }
-
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
+                updateQualityForExpiredItem(item);
             }
         }
     }
